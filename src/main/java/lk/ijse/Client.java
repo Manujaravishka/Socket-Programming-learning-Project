398249bf078cea1723d2a1aff1395de4024962ea
Manujaravishka <manujaravishka2004@gmail.com>
2025-10-08 11:55:04 +0530

    Secnd Commit(client typing input Terminal )

--- src/main/java/lk/ijse/Client.java
+++ src/main/java/lk/ijse/Client.java
@@ -4,12 +4,16 @@ package lk.ijse;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) {
         try {
             Socket remoteSocket = new Socket("localhost",3000);
             DataOutputStream dataOutputStream = new DataOutputStream(remoteSocket.getOutputStream());
+            Scanner scanner = new Scanner(System.in);
+            String msg = scanner.nextLine();
+            dataOutputStream.writeUTF("Typing input : "+msg);
 
             dataOutputStream.writeUTF("Hello Server");
             dataOutputStream.flush();
